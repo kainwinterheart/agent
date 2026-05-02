d2112bab6506c165059cb3995886927855020d42
Kain Winterheart <kain.winterheart@gmail.com>
2026-05-02 13:45:01 +0100

    update wrapper

--- codex.sh
+++ codex.sh
@@ -1,23 +1,29 @@
 #!/bin/sh -e
 
+container="${container:-docker.io/node:25-slim}" # $(podman build -f Dockerfile -q)
+
 codex="${codex_interactive}"
 
 if [ ! -z "${AC_AGENT_NAME}" ]; then
     codex="${codex_auto}"
 
-    if [ "${AC_AGENT_NAME}" != 'coder' ]; then
+    if [ "${AC_AGENT_NAME}" != 'coder' ] || [ ! -z "${isolate_coder}" ]; then
         spec=""
         spec="${spec} -v /tmp:/tmp:O"
         spec="${spec} -v ${HOME}:${HOME}:O"
-        spec="${spec} -v ${PWD}:${PWD}:O"
+
+        pwd_mount_suffix=''
+        if [ "${AC_AGENT_NAME}" != 'coder' ]; then
+            pwd_mount_suffix=':O'
+        fi
+        spec="${spec} -v ${PWD}:${PWD}${pwd_mount_suffix}"
 
         x="${HOME}/.codex"
         spec="${spec} -v $x:$x"
 
-        container="docker.io/node:25-slim" # $(podman build -f Dockerfile -q)
-        codex="podman run -e HOME=${HOME} --rm ${spec} -i ${container} ${codex} --cd ${PWD}"
+        codex="podman run -e HOME=${HOME} --rm ${spec} -i ${container} ${codex}"
     fi
 fi
 
 set -x
-exec $codex "$@"
+exec $codex --cd ${PWD} "$@"
