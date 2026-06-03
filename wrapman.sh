b17efa94386e307cb09a8b4729b9233e6f94aca0
Kain Winterheart <kain.winterheart@gmail.com>
2026-06-03 16:24:32 +0100

    update wrapper

--- wrapman.sh
+++ wrapman.sh
@@ -1,13 +1,20 @@
 #!/bin/sh -e
 
-wrapman_pause="${wrapman_pause:-10}"
-
 lockfile="$1"
 shift
 
 container_name="$1"
 shift
 
-nohup /bin/sh -c "sleep ${wrapman_pause} ; flock ${lockfile} podman stop ${container_name} ; unlink ${lockfile}" > /dev/null &
+wrapman_pause="${wrapman_pause:-10}"
+timeout_cmd=${timeout_cmd:-$(pstree -A -a -s -l $$ |grep -oE "\btimeout\s+(-s\s+[0-9A-Z]+\s+)?[0-9a-zA-Z]+" | tail -1)}
 
-exec flock "${lockfile}" podman run --name="${container_name}" "$@"
+nohup /bin/sh -c "sleep ${wrapman_pause} ; flock ${lockfile} podman stop ${container_name} ; unlink ${lockfile}" >/dev/null &
+
+if [ -t 1 ]; then
+    if [ ! -z "${timeout_cmd}" ]; then
+        timeout_cmd=$(echo "${timeout_cmd}" | sed -e 's/^timeout/timeout --foreground/')
+    fi
+fi
+
+exec flock "${lockfile}" ${timeout_cmd} podman run --name="${container_name}" "$@"
