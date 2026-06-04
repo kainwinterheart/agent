e6d99707e9c6848c40af9966f49e01cc27fb27cc
Kain Winterheart <kain.winterheart@gmail.com>
2026-06-04 13:54:15 +0100

    fix background cleanup

--- wrapman.sh
+++ wrapman.sh
@@ -9,7 +9,7 @@ shift
 wrapman_pause="${wrapman_pause:-10}"
 timeout_cmd=${timeout_cmd:-$(pstree -A -a -s -l $$ |grep -oE "\btimeout\s+(-s\s+[0-9A-Z]+\s+)?[0-9a-zA-Z]+" | tail -1)}
 
-nohup /bin/sh -c "sleep ${wrapman_pause} ; flock ${lockfile} podman stop ${container_name} ; unlink ${lockfile}" >/dev/null &
+setsid -f /bin/sh -c "sleep ${wrapman_pause} ; flock ${lockfile} podman stop ${container_name} ; unlink ${lockfile}" &>/dev/null
 
 if [ -t 1 ]; then
     if [ ! -z "${timeout_cmd}" ]; then
